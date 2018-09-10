f584edad8c7c13cea1b23ae50bdfcbc14a9dd8d6
Rodrigo Baroni <rodrigobaroni>
2018-09-10 08:38:00 -0300

    Initial Commit

---
+++ Rochdale FC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rochdale FC
+//
+//  Created by Rodrigo Baroni on 10/09/2018.
+//  Copyright © 2018 Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
