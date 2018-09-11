caebe428a88a43917b0d3681a8d875261b80331b
Rodrigo Baroni <rodrigobaroni>
2018-09-11 16:22:32 -0300

    2º – Commit : Criação de telas

---
+++ Rochdale FC/Historico_TVC.swift
@@ -0,0 +1,24 @@
+//
+//  Historico_TVC.swift
+//  Rochdale FC
+//
+//  Created by Rodrigo Baroni on 11/09/2018.
+//  Copyright © 2018 Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class Historico_TVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Rochdale FC/Placar_TVC.swift
@@ -0,0 +1,24 @@
+//
+//  Placar_TVC.swift
+//  Rochdale FC
+//
+//  Created by Rodrigo Baroni on 10/09/2018.
+//  Copyright © 2018 Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class Placar_TVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
